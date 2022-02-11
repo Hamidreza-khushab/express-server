e5b4cb81a4445ebc64103b6c39432245585d24d2
Hamidreza <qiisohamid@gmail.com>
2022-02-11 11:26:05 +0100

    add external port in .env

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const express = reqiure('express');
+const morgan = reqiure('morgan');
+
+const app = express();
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use('/', home);
+app.use('/listproducts', listproducts);
+app.use('/users', users);
+app.use('/orders', orders);
+app.use('/contact', contact);
+app.listen(port, ( err ) =>
+{
+    if(err)
+        console.log(err);
+    else
+        console.log('app run in port '+ port);
+});
