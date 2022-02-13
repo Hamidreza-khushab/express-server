088bcc958c1cd921d42ce5f320ad5e7f7693bdce
Hamidreza <qiisohamid@gmail.com>
2022-02-13 19:38:03 +0100

    start routes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = reqiure('express');
-const morgan = reqiure('morgan');
+const express = require('express');
+const morgan = require('morgan');
 
-const port = prosses.env.PORT || 4000;
+require('dotenv').config();
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.use('/', home);
-app.use('/listproducts', listproducts);
-app.use('/users', users);
-app.use('/orders', orders);
-app.use('/contact', contact);
+// app.use('/', home);
+// app.use('/listproducts', listproducts);
+// app.use('/users', users);
+// app.use('/orders', orders);
+// app.use('/contact', contact);
 app.listen(port, ( err ) =>
 {
     if(err)
