34e4830b1967a77b789b8c9ef25ed0e6b2487d41
Hamidreza <qiisohamid@gmail.com>
2022-02-13 20:08:39 +0100

    add homr ruter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const home = require('./routes/home.js');
+
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 
@@ -8,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// app.use('/', home);
+app.use('/', home);
 // app.use('/listproducts', listproducts);
 // app.use('/users', users);
 // app.use('/orders', orders);
